29f5869390ffe1cd64a71dbac18ac40566bc837b
nathaniel <nathaniel.simard.42@gmail.com>
2022-06-09 14:38:41 -0400

    ci: wait for crates to be available

--- ci/publish.sh
+++ ci/publish.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+echo "Publishing dose-derive ..."
 cd dose-derive/
 cargo publish --token ${CRATES_IO_API_TOKEN}
+echo "Sucessfully published dose-derive"
+
+echo "Waiting 10 seconds for dose-derive to be available ..."
+sleep 10
+
+echo "Publishing dose ..."
 cd ../
 cargo publish --token ${CRATES_IO_API_TOKEN}
+echo "Sucessfully published dose"
